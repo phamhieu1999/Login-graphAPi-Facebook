61e98d95e9745b0515cf6ebf640157c7f79479ec
pham  van hieu <hieupvd00585@fpt.edu.vn>
2021-05-18 21:08:45 +0700

    add router

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports={
+  "facebook_api_key"      :     "318283186310051",
+  "facebook_api_secret"   :     "3cc217caa6ec6f131e704fca1e73db0c",
+  "callback_url"          :     "http://localhost:3000/auth/facebook/callback",
+  "use_database"          :     true,
+  "host"                  :     "localhost",
+  "username"              :     "root",
+  "password"              :     "",
+  "database"              :     "login_passport"
+}
--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    googleClientID:
-      "318283186310051",
-    googleClientSecret: "3cc217caa6ec6f131e704fca1e73db0c"
-  };
